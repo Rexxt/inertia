1907b3d4a82a3f67a2feada55c1a1a2831bb7dc0
Mizu <40831463+Rexxt@users.noreply.github.com>
2023-05-01 16:07:32 +0200

    Proper initial commit
    Application can now read pack files locally, create the UI for pack files, download installers for the specified software.
    
    Auto-install and URL imports will follow. Pack updating will follow later.

---
+++ downloader.py
@@ -0,0 +1,39 @@
+import os, json, tkinter as tk, requests
+from tkinter import ttk
+from tkinter import messagebox
+
+class DownloaderWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # root window
+        self.title("Pack Downloader")
+        self.geometry('400x300')
+        self.style = ttk.Style(self)
+        self.style.theme_use('vista')
+
+        self.list = tk.Listbox(self)
+        self.list.pack(side='top', expand=True, fill='both')
+        self.progress_var = tk.IntVar()
+        self.progress = ttk.Progressbar(self)
+        self.progress.pack(side='bottom', expand=True, fill='both')
+
+    def download_cat(self, pack_name, name, programs):
+        self.list.delete(0, tk.END)
+        self.title("Downloading and installing from " + name + " in " + pack_name)
+
+        i = 1
+        for program in programs:
+            self.list.insert(i, program['name'])
+
+        self.progress['maximum'] = len(programs)
+        self.progress_var.set(0)
+
+        for program in programs:
+            r = requests.get(program['installer'], allow_redirects=True)
+            if not os.path.isdir('installers/' + pack_name + '/' + name):
+                os.makedirs('installers/' + pack_name + '/' + name)
+            open('installers/' + pack_name + '/' + name + '/' + program['name'] + '.exe', 'wb').write(r.content)
+            self.progress.step(1)
+
+        messagebox.showinfo(name + " in " + pack_name, 'Installers downloaded.')
+        self.destroy()
--- inertia.py
+++ inertia.py
@@ -1,32 +1,100 @@
 import os, json, tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog as fd
+import downloader
 
 INERTIA_VERSION = [0, 1, 0, "alpha"]
 INERTIA_VERSION_STR = [str(e) for e in INERTIA_VERSION]
 
-root = tk.Tk()
-root.title("Inertia version " + ".".join(INERTIA_VERSION_STR))
-root.geometry("400x300")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # root window
+        self.title("Inertia version " + ".".join(INERTIA_VERSION_STR))
+        self.geometry('400x300')
+        self.style = ttk.Style(self)
+        self.style.theme_use('vista')
 
-control_menu = tk.Menu(root, tearoff=0)
-root.config(menu=control_menu)
-inertia_menu = tk.Menu(control_menu, tearoff=0)
-control_menu.add_cascade(label='Inertia', menu=inertia_menu)
-inertia_menu.add_command(label='Exit', command=exit)
+        self.inertia_pack = {}
+        self.categories = {}
 
-tab_control = ttk.Notebook(root)
+        control_menu = tk.Menu(self, tearoff=0)
+        self.config(menu=control_menu)
+        inertia_menu = tk.Menu(control_menu, tearoff=0)
+        control_menu.add_cascade(label='Inertia', menu=inertia_menu)
+        inertia_menu.add_command(label='Exit', command=exit)
 
-tab1 = ttk.Frame(tab_control)
-tab2 = ttk.Frame(tab_control)
+        packs_menu = tk.Menu(control_menu, tearoff=0)
+        control_menu.add_cascade(label='Software Pack', menu=packs_menu)
+        packs_menu.add_command(label='Open from file', command=self.import_pack_file)
 
-tab_control.add(tab1, text ='Gaming')
-tab_control.add(tab2, text ='Video')
-tab_control.pack(expand = 1, fill = "both")
-ttk.Label(tab1, 
-          text = "Steam n shit").grid(column = 0, 
-                               row = 0,)  
-ttk.Label(tab2,
-          text = "OBS n shit").grid(column = 0,
-                                    row = 0)
-  
-root.mainloop()
+        self.tab_control = ttk.Notebook(self)
+        self.tab_control.pack(expand = 1, fill = "both")
+        
+    def build_pack(self):
+        self.title("Inertia version " + ".".join(INERTIA_VERSION_STR) + " - " + self.inertia_pack['name'])
+        for tab in self.tab_control.tabs():
+            self.tab_control.forget(tab)
+        self.categories = {}
+        for category in self.inertia_pack['programs']:
+            self.categories[category] = {}
+            self.categories[category]['tab'] = ttk.Frame(self.tab_control)
+            self.tab_control.add(self.categories[category]['tab'], text=category)
+
+            self.categories[category]['btns'] = {}
+            self.categories[category]['btns']['installCat'] = tk.Button(self.categories[category]['tab'], text='Install checked', command=self.start_downloader)
+            self.categories[category]['btns']['installCat'].grid(row=0, column=1, sticky="NE")
+            self.categories[category]['btns']['uncheckAll'] = tk.Button(self.categories[category]['tab'], text='Uncheck all', command=self.cat_uncheck_all)
+            self.categories[category]['btns']['uncheckAll'].grid(row=2, column=1, sticky="NE")
+            self.categories[category]['btns']['checkAll'] = tk.Button(self.categories[category]['tab'], text='Check all', command=self.cat_check_all)
+            self.categories[category]['btns']['checkAll'].grid(row=3, column=1, sticky="NE")
+            
+            self.categories[category]['boxes'] = {}
+            self.categories[category]['vars'] = {}
+            row_index = 0
+            for program in self.inertia_pack['programs'][category]:
+                if self.inertia_pack['programs'][category][program]['default']:
+                    self.categories[category]['vars'][program] = tk.IntVar(value=1)
+                else:
+                    self.categories[category]['vars'][program] = tk.IntVar(value=0)
+                self.categories[category]['boxes'][program] = tk.Checkbutton(
+                    self.categories[category]['tab'],
+                    text=program + ' (' + self.inertia_pack['programs'][category][program]['version'] + ')',
+                    variable=self.categories[category]['vars'][program])
+                self.categories[category]['boxes'][program].grid(row=row_index, column=0, sticky="NW")
+                row_index += 1
+        
+            self.categories[category]['tab'].columnconfigure(1, weight=1)
+            self.categories[category]['tab'].rowconfigure(1, weight=1)
+        messagebox.showinfo('Inertia', 'Pack "' + self.inertia_pack['name'] + '" imported successfully.')
+
+    def cat_uncheck_all(self):
+        selected_category = self.tab_control.tab(self.tab_control.select(), 'text')
+        for program in self.categories[selected_category]['boxes']:
+            self.categories[selected_category]['vars'][program].set(0)
+
+    def cat_check_all(self):
+        selected_category = self.tab_control.tab(self.tab_control.select(), 'text')
+        for program in self.categories[selected_category]['boxes']:
+            self.categories[selected_category]['vars'][program].set(1)
+
+    def start_downloader(self):
+        selected_category = self.tab_control.tab(self.tab_control.select(), 'text')
+        programs_list = []
+        for program in self.inertia_pack['programs'][selected_category]:
+            if self.categories[selected_category]['vars'][program].get() == 1:
+                programs_list.append({'name': program, 'installer': self.inertia_pack['programs'][selected_category][program]['installer']})
+        window = downloader.DownloaderWindow()
+        window.grab_set()
+        window.download_cat(self.inertia_pack['name'], selected_category, programs_list)
+
+    def import_pack_file(self):
+        filename = fd.askopenfilename(title="Import Inertia pack", filetypes=(('JSON', '*.json'),("All files", "*.*")))
+        with open(filename, 'r') as f:
+            self.inertia_pack = json.loads(f.read())
+        self.build_pack()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
